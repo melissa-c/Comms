32f4fd048ba9f7fee91846ca5cce826bdc37f3f5
LucyMoore <lucyviolet.moore@gmail.com>
2016-04-15 14:58:56 +1200

    set up routes

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 //import react and react-dom 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Router, Route, hashHistory} from 'react-router'
 //import homepage button component
-import HomePageBtn from './src/components/homePageBtn'
+//import HomePageBtn from './src/components/homePageBtn'
+import HomePage from './src/components/homepage'
+
 
 var btnNames = ['Schedule', 'I would like...', 'I can...', 'Social Stories']
 var buttons = btnNames.map(function(name, id){
@@ -11,7 +14,11 @@ var buttons = btnNames.map(function(name, id){
 });
 
 
-	ReactDOM.render(
-		<div> {buttons} </div>, document.getElementById('content')
-
+	ReactDOM.render((
+		<Router history={hashHistory}> 
+		<Route path='homepage' component={HomePage}/>
+		</Router>
+		), document.getElementById('content')
 )
+
+
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -3,14 +3,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-module.exports = class HomePageBtn extends React.Component {
+module.exports = class HomePage extends React.Component {
   constructor(props){
    super(props)
   }
   render (){
-   return  <div><button className='navBtn'>{this.props.txt}</button></div>
+   return  <div>Home Page Route</div>
       
   }
 }
-
 
