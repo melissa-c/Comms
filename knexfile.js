11ded128647f35b015107bc4eed553035aa6cc32
mel <dyslixec@hotmail.com>
2016-04-15 16:42:08 +1200

    add .env, install dotenv, update knexfile

--- knexfile.js
+++ knexfile.js
@@ -3,41 +3,50 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'postgresql',
     connection: {
-      filename: './dev.sqlite3'
+      database: 'commspics',
+      user:     process.env.PG_DATABASE_USER,
+      password: process.env.PG_DATABASE_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'gallery'
     }
   },
 
   staging: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: 'commspics',
+      user:     'admin',
+      password: 'LordByron'
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'gallery'
     }
   },
 
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: 'commspics',
+      user:     'admin',
+      password: 'LordByron'
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'gallery'
     }
   }
 
