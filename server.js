dfa294dd8df97f9fc1aa55779656ab3644b07f68
LucyMoore <lucyviolet.moore@gmail.com>
2016-04-18 17:28:02 +1200

    code recatored

--- server.js
+++ server.js
@@ -1,42 +1,24 @@
 var express = require('express')
 var app = express()
-var knex = require('knex')
-var pg = require('pg')
-var accessDB = 'postgres://testuser:test@localhost/commspics'
+var Knex = require('knex')
+var knexConfig = require('./knexfile')
+var knex = Knex(knexConfig[process.env.NODE_ENV || 'development'])
 
-var Client = new pg.Client(accessDB)
 app.use(express.static('client'))
 
-console.log('server')
-
-
-
-function getDB(callback){
-	console.log('inside getDB')
-	Client.connect(function(err){
-		if(err){
-			return console.log('connection error', err)
-		}
-		Client.query('SELECT * FROM gallery', function(err, result){
-			if(err){
-				return console.log('error reciving data', err)
-			} else{
-			callback(null, result.rows)
-			}
-			Client.end()
-		})
-
-	})
-}
-
 app.get('/database', function(req,res){
-  getDB(function(err, data){
+  knex.select().table('gallery')
+  .then(function(data){
   	res.send(data)
-
+  })
+  .catch(function(error){
+  	console.log("Error: ", error)
   })
 })
+
 app.set('port', 3000)
+
 var server = app.listen(app.get('port'), function() {
-var port = server.address().port;
+	var port = server.address().port
 })
 
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -2,30 +2,28 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import HomePageBtn from './homePageBtn'
-import Header from './header'
 import {Router, Route} from 'react-router'
 
+import HomePageBtn from './homePageBtn'
+import Header from './header'
 
 function goToAskPage(){
   window.location = "/#/askpage"
 }
-
 
 module.exports = class HomePage extends React.Component {
   constructor(props){
    super(props)
   }
   render (){
-   return  (
+    return  (
       <div>
         <Header />
-        
         <HomePageBtn txt={"Schedule"} img={"icon/clipboard.png"} />
         <HomePageBtn txt={"I would like..."} onClick={goToAskPage} img={"icon/question.png"} />
         <HomePageBtn txt={"I can..."} img={"icon/user.png"} />
       </div>
-      )
+    )
   }
 }
 
