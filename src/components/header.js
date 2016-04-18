c204305b4f2464db129d362cf28afba928975611
LucyMoore <lucyviolet.moore@gmail.com>
2016-04-18 19:36:10 +1200

    minimal changes

--- src/components/header.js
+++ src/components/header.js
@@ -11,45 +11,6 @@ function back(){
   window.location = '/#/';
 }
 
-
-
-function counter(){
-  document.getElementById('timerDiv').className = "timerDivVisible"
-  var loader = document.getElementById('loader')
-//   ,   border = document.getElementById('border')
-  ,   alpha  = 0
-  ,        π = Math.PI
-  ,        t = prompt('Time');
-
-    (function draw() {
-      alpha++;
-      //alpha %= 360;
-      var r = ( alpha * π / 180 )
-      var x = Math.sin( r ) * 200
-      var y = Math.cos( r ) * - 200
-      var mid = ( alpha >= 180 ) ? 1 : 0
-      var anim = 'M 0 0 v -200 A 200 200 1 ' 
-               + mid + ' 1 ' 
-               +  x  + ' ' 
-               +  y  + ' z';
-      //[x,y].forEach(function( d ){
-      //  d = Math.round( d * 1e3 ) / 1e3;
-      //});
-           
-      if(alpha <360) {
-        setTimeout(draw, t) // Redraw
-      } else {
-          var animate ="M 0 0 v -200 A 200 200 1 1 1 -.1 -200 z";
-         // alert("Thanks for waiting");
-      }
-        loader.setAttribute( 'd', anim );
-        border.setAttribute( 'd', anim );
-    })()
-}
-
-
-
-
 module.exports = class Header extends React.Component {
   constructor(props){
    super(props)
