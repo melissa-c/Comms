14667d6a57c1c6122c8a60a830365e86ba3a2c18
mel <dyslixec@hotmail.com>
2016-04-19 12:46:05 +1200

    display buttons on homepage

---
+++ src/components/headerBtn.js
@@ -0,0 +1,22 @@
+'use strict'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+module.exports = class HeaderBtn extends React.Component {
+  constructor(props){
+   super(props)
+  }
+  render (){
+    return  (
+      <div>
+        <button className='headerBtn' onClick={this.props.onClick}>
+          <img src={this.props.img} />
+          <p className="labels">{this.props.txt}</p>
+        </button>
+      </div>
+    )
+  }
+}
+
+
