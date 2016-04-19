f3d96d723aa0625aa5b56ae3d9ad076984b30dcc
mel <dyslixec@hotmail.com>
2016-04-20 11:17:22 +1200

    recent imgs still nt working

--- src/components/askpage.js
+++ src/components/askpage.js
@@ -22,28 +22,28 @@ module.exports = class AskPage extends React.Component {
   }
  
   update(e){
-/*    for(var i = 0; i < recentArray.length; i++){
-      if(recentArray[i] === [e.target.src, e.target.className]){
+    recentArray.forEach(function(item){
+      if(recentArray.indexOf([e.target.src, e.target.className])){
             // console.log('array', [e.target.src, e.target.className])
             // console.log('array', recentArray)
         return recentArray
-      } else if (recentArray[i] !== [e.target.src, e.target.className] && recentArray.length < 5){
+      } else if (!recentArray.indexOf([e.target.src, e.target.className]) && recentArray.length < 5){
         recentArray.push([e.target.src, e.target.className])
             // console.log('push', [e.target.src, e.target.className])
             // console.log('push', recentArray)
-      } else {
+      } else if (!recentArray.indexOf([e.target.src, e.target.className]) && recentArray.length >= 5){
         recentArray.shift()
         recentArray.push([e.target.src, e.target.className])
             // console.log('shift', [e.target.src, e.target.className])
             // console.log('shift', recentArray)
       }
-    }
+    })
     this.setState(
       {imageURL: e.target.src, recentURL: recentArray}
     )
   }
-*/    
-  if(recentArray.indexOf([e.target.src, e.target.className])){
+    
+/*  if(recentArray.indexOf([e.target.src, e.target.className])){
       return recentArray
     } else if(recentArray.length < 5){
       recentArray.push([e.target.src, e.target.className])
@@ -55,7 +55,7 @@ module.exports = class AskPage extends React.Component {
       {imageURL: e.target.src, recentURL: recentArray}
     )
   }
-
+*/
 /*    if(recentArray.length>=5){
       recentArray.shift()
       recentArray.push([e.target.src, e.target.className])
