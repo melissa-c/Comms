14901d3cc6d6b88bc6f00ade81b15b2d79061d4e
mel <dyslixec@hotmail.com>
2016-04-20 22:00:40 +1200

    jump buttons work

--- src/components/imgGallery.js
+++ src/components/imgGallery.js
@@ -6,8 +6,8 @@ import Request from 'superagent'
 
 module.exports = class ImgGallery extends React.Component {
   constructor(props){
-   super(props)
-   this.state = {images: []}
+    super(props)
+    this.state = {images: []}
   }
 
   componentDidMount(){
@@ -15,75 +15,96 @@ module.exports = class ImgGallery extends React.Component {
     .get('/database')
     .end(function(err, res){
       this.setState(
-        {images: JSON.parse(res.text)}
+        {images: JSON.parse(res.text), filter: "ALL"}
         )
     }.bind(this))
   }
 
+  update(e){
+  }
+
   render (){
     if(this.props.sender==="askpage"){
-      return  (
+      return (
         <div>
           <div className="imgGallery">
-            {this.state.images.map(function(image){
+            {this.state.images.filter(function(image){
+              if(this.state.filter == "ALL"){
+                return true
+              } else {
+              return image.category == this.state.filter}
+            }, this).map(function(image){
                     
               switch(image.category) {
                 
                 case "food":
-                    return(<img className= "indAskImg Orange" src={image.filepath} onClick={this.props.onClick}></img>)
-                    break;
+                  return(<img className= "indAskImg Orange" src={image.filepath} onClick={this.props.onClick}></img>)
+                  break;
                 case "home":
-                    return(<img className= "indAskImg Yellow" src={image.filepath} onClick={this.props.onClick}></img>)
-                    break;
+                  return(<img className= "indAskImg Yellow" src={image.filepath} onClick={this.props.onClick}></img>)
+                  break;
                 case "outside":
-                    return(<img className= "indAskImg Green" src={image.filepath} onClick={this.props.onClick}></img>)
-                    break;
+                  return(<img className= "indAskImg Green" src={image.filepath} onClick={this.props.onClick}></img>)
+                  break;
                 case "school":
-                    return(<img className= "indAskImg Red" src={image.filepath} onClick={this.props.onClick}></img>)
-                    break;
+                  return(<img className= "indAskImg Red" src={image.filepath} onClick={this.props.onClick}></img>)
+                  break;
               }
             }, this)}
           
           </div>
-          <button className='yellow'></button>
-          <button className='blue'></button>
-          <button className='red'></button>
-          <button className='green'></button>
+          
+          <button name='yellow' className='btnJumpYellow' onClick={function(){this.setState({filter: "home"})}.bind(this)}>home</button>
+          <button name='green' className='btnJumpGreen' onClick={function(){this.setState({filter: "outside"})}.bind(this)}>outside</button>
+          <button name='red' className='btnJumpRed' onClick={function(){this.setState({filter: "school"})}.bind(this)}>school</button>
+          <button name='orange' className='btnJumpOrange' onClick={function(){this.setState({filter: "food"})}.bind(this)}>food</button>
         </div>
       )
     }
-    else{
-      return  (
-          <div>
-            <div className="imgGallery">
-              {this.state.images.map(function(image){
-                      
-                switch(image.category) {
-                  case "body":
-                    return(<img className= "indAskImg Purple" src={image.filepath} onClick={this.props.onClick}></img>)
-                    break;
-                  case "emotions":
-                    return(<img className= "indAskImg Blue" src={image.filepath} onClick={this.props.onClick}></img>)
-                    break;
-                  case "home":
-                      return(<img className= "indAskImg Yellow" src={image.filepath} onClick={this.props.onClick}></img>)
-                      break;
-                  case "outside":
-                      return(<img className= "indAskImg Green" src={image.filepath} onClick={this.props.onClick}></img>)
-                      break;
-                  case "school":
-                      return(<img className= "indAskImg Red" src={image.filepath} onClick={this.props.onClick}></img>)
-                      break;
-                }
-              }, this)}
-            </div>
-            <button className='yellow'></button>
-            <button className='blue'></button>
-            <button className='red'></button>
-            <button className='green'></button>
+    else {
+      return (
+        <div>
+          <div className="imgGallery">
+            {this.state.images.filter(function(image){
+              if(this.state.filter == "ALL"){
+                return true
+              } else {
+              return image.category == this.state.filter}
+            }, this).map(function(image){
+                    
+              switch(image.category) {
+                
+                case "emotions":
+                  return(<img className= "indAskImg Blue" src={image.filepath} onClick={this.props.onClick}></img>)
+                  break;
+                case "body":
+                  return(<img className= "indAskImg Purple" src={image.filepath} onClick={this.props.onClick}></img>)
+                  break;
+                case "food":
+                  return(<img className= "indAskImg Orange" src={image.filepath} onClick={this.props.onClick}></img>)
+                  break;
+                case "home":
+                  return(<img className= "indAskImg Yellow" src={image.filepath} onClick={this.props.onClick}></img>)
+                  break;
+                case "outside":
+                  return(<img className= "indAskImg Green" src={image.filepath} onClick={this.props.onClick}></img>)
+                  break;
+                case "school":
+                  return(<img className= "indAskImg Red" src={image.filepath} onClick={this.props.onClick}></img>)
+                  break;
+              }
+            }, this)}
+          
           </div>
-          )
-
+          
+          <button name='blue' className='btnJumpBlue' onClick={function(){this.setState({filter: "emotions"})}.bind(this)}>emotions</button>
+          <button name='purple' className='btnJumpPurple' onClick={function(){this.setState({filter: "body"})}.bind(this)}>body</button>
+          <button name='yellow' className='btnJumpYellow' onClick={function(){this.setState({filter: "home"})}.bind(this)}>home</button>
+          <button name='green' className='btnJumpGreen' onClick={function(){this.setState({filter: "outside"})}.bind(this)}>outside</button>
+          <button name='red' className='btnJumpRed' onClick={function(){this.setState({filter: "school"})}.bind(this)}>school</button>
+          <button name='orange' className='btnJumpOrange' onClick={function(){this.setState({filter: "food"})}.bind(this)}>food</button>
+        </div>
+      )
     }
   }
 }
